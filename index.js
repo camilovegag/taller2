dbcc62532548a345c8089ab1a12ab220afc8c6cc
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2020-02-03 23:30:11 -0500

    Subí el taller

---
+++ index.js
@@ -0,0 +1,7 @@
+function jugar() {
+  document.querySelector(".btn").innerHTML = "Playing";
+  document.getElementById("div1").setAttribute("id", "div2");
+  document.getElementById("div3").setAttribute("id", "div4");
+  document.getElementById("div5").setAttribute("id", "div6");
+  document.getElementById("div7").setAttribute("id", "div8");
+}
